f5c5ba6fcaf3c1ad45962aebd7e1ea5a3eb44baf
SriramPrakash <sriramprakash3@gmail.com>
2017-10-23 09:18:22 +0530

    Making changes to server	modified:   LionService/server/server.js Making changes to server	new file:   LionService/utils/checkauth.js Checkin new file:   node_modules/lodash.findindex/LICENSE Checkin	new file:   node_modules/lodash.findindex/README.md Checkin	new file:   node_modules/lodash.findindex/index.js Checkin	new file:   node_modules/lodash.findindex/package.json Checkin	modified:   package.json

--- LionService/server/server.js
+++ LionService/server/server.js
@@ -2,6 +2,7 @@ var express=require('express');
 var bodyparser=require('body-parser');
 var app=express();
 var _=require('lodash');
+var findIndex = require('lodash.findindex');
 
 app.use(express.static('views'));
 app.use(bodyparser.urlencoded({extended:true}));
@@ -9,34 +10,66 @@ app.use(bodyparser.json());
 //console.log(app);
 
 var lions=[];
+
 var id=0;
+
+
+
+app.param('id',function(req,res,next,Id){	
+	if(Id !=null){		
+		console.log(lions)
+		var lion=findIndex(lions,{"name":'Sriram'});	
+		console.log(lion);
+			if(lion){
+				next(new Error('Not Found'));
+			}	
+		req.body= lions[lion];		
+	}	
+	next();
+});
 
 app.get('/lions',function(req,res){
 	res.json(lions);
 });
 
 app.get('/lions/:id',function(req,res){
-	res.json(_.find(lions,{id:req.params.id})||{});
+	res.json(req.body)
 });
 
-app.post('/lions',function(req,res){
-	var lion = req.body;
-	id++;
-	lion.id=id;
-	lions.push(lion);
-	res.json(lion);
-});
+app.post('/lions',function(req,res,next){
+			var lion=req.body;
+			if(lion.id!=null){			
+				if(!lions[lion.id]){
+					id++;
+					lion.id=id;
+					lions.push(lion);
+				}
+				else{
+					lions[lion.id].name=lion.name;
+					lions[lion.id].pride=lion.pride;
+					lions[lion.id].type=lion.type;
+				}
+						
+		}
+		else{
+			id++;
+			lion.id=id;
+			lions.push(lion);
+		}
+		res.json(lion)
+	});
 
 app.put('/lions/:id',function(req,res){
-	var lion = _.find(lions,{id:req.params.id});
-	if(!lions[lion]){
-		res.send();
-	}
-	else{
-		next(err);
-		var deletedLion = lions[lion];
-		lions.splice(lion,1);
-		res.json(deletedLion);
+
+	var deletedLion = req.body.lion;
+	lions.splice(deletedLion.id,1);
+	res.json(deletedLion);
+	
+});
+
+app.use(function(err){
+	if(err){
+		console.log(err);
 	}
 });
 
